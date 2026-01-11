3b562dc84caf19fb72e2f6eda88a671d8fa56eef
Rohith Vinod <idliyout@gmail.com>
2026-01-12 02:42:51 +0530

    [refactor] significantly reduce LOC

--- src/main.rs
+++ src/main.rs
@@ -1,225 +1,142 @@
-use ::std::{
+use serde_derive::Deserialize;
+use std::{
+    cmp::Ordering,
     collections::HashMap,
-    env,
-    fs::{read_to_string, File},
-    io::{ErrorKind, Write},
+    env, fs,
+    io::Write,
     process::{exit, Command, Stdio},
     time::SystemTime,
 };
-use serde_derive::Deserialize;
-use std::cmp::Ordering;
-
-#[derive(Debug, Deserialize)]
-struct Recipe {
-    build: Build,
-    custom: Option<HashMap<String, Custom>>,
-    pre: Option<HashMap<String, Pre>>,
-    env: Option<HashMap<String, String>>,
-    debug: Option<bool>,
-}
-
-impl Recipe {
-    fn new() -> Option<Self> {
-        if let Err(e) = File::open("recipe.toml") {
-            if e.kind() == ErrorKind::NotFound {
-                printb!("Could not find a recipe.toml, generating one.");
-                let mut file = File::create("recipe.toml").unwrap();
-                file.write_all(b"[build]\ncmd = \"\"").unwrap();
-                if env::current_dir().is_ok() {
-                    printb!("Generated file in {:?}", env::current_dir().unwrap());
-                } else {
-                    printb!("Generated file.");
-                }
-                exit(0);
-            } else {
-                printb!("Error: {}", e);
-            }
-        }
-
-        let mut recipe_str = String::new();
-
-        match read_to_string("recipe.toml") {
-            Ok(s) => recipe_str.push_str(&s),
-            Err(e) => {
-                printb!("Error: {}", e);
-                exit(1);
-            }
-        }
-
-        let recipe: Recipe = match toml::from_str(&recipe_str) {
-            Ok(r) => r,
-            Err(e) => {
-                printb!("Error: {}", e);
-                exit(1);
-            }
-        };
-        Some(recipe)
-    }
-}
-
-trait Runnable {
-    fn execute(&self, name: String, debug: bool);
-}
-
-#[derive(Debug, Deserialize)]
-struct Build {
-    cmd: String,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-struct Custom {
-    cmd: String,
-    run: bool,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-struct Pre {
-    cmd: String,
-}
-
-impl Runnable for Build {
-    fn execute(&self, name: String, debug: bool) {
-        if self.cmd.is_empty() {
-            printb!("Build command is empty.");
-            exit(1);
-        }
-
-        run_cmd(name, self.cmd.to_string(), debug);
-    }
-}
-
-impl Runnable for Custom {
-    fn execute(&self, name: String, debug: bool) {
-        if self.cmd.is_empty() {
-            printb!("Custom command is empty.");
-            exit(1);
-        }
-
-        run_cmd(name, self.cmd.to_string(), debug);
-    }
-}
-
-impl Runnable for Pre {
-    fn execute(&self, name: String, debug: bool) {
-        if self.cmd.is_empty() {
-            printb!("Pre command is empty.");
-            exit(1);
-        }
-
-        run_cmd(name, self.cmd.to_string(), debug);
-    }
-}
 
 #[macro_export]
 macro_rules! printb {
-    ($($arg:tt)*) => {
-        println!("\x1b[32mBaker:\x1b[0m {}", format!($($arg)*));
-    };
+    ($($arg:tt)*) => { println!("\x1b[32mBaker:\x1b[0m {}", format!($($arg)*)); };
+}
+
+#[derive(Debug, Deserialize)]
+struct Task {
+    cmd: String,
+    #[serde(default)]
+    run: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct Recipe {
+    build: Task,
+    #[serde(default)]
+    custom: HashMap<String, Task>,
+    #[serde(default)]
+    pre: HashMap<String, Task>,
+    #[serde(default)]
+    env: HashMap<String, String>,
+    #[serde(default)]
+    debug: bool,
+}
+
+impl Recipe {
+    fn new() -> Self {
+        let path = "recipe.toml";
+        let s = fs::read_to_string(path).unwrap_or_else(|_| {
+            printb!("Could not find recipe.toml, generating one.");
+            let mut f = fs::File::create(path).unwrap();
+            f.write_all(b"[build]\ncmd = \"\"").unwrap();
+            exit(0);
+        });
+
+        toml::from_str(&s).unwrap_or_else(|e| {
+            printb!("Error: {}", e);
+            exit(1)
+        })
+    }
+
+    fn execute(&self, name: &str, task: &Task) {
+        if task.cmd.is_empty() {
+            return;
+        }
+
+        if self.debug {
+            let start = SystemTime::now();
+            if name == "build" {
+                printb!("Running build command.");
+            } else {
+                printb!("Running hook \"{}\".", name);
+            }
+            self.shell(&task.cmd);
+            printb!("Finished in {}ms.", start.elapsed().unwrap().as_millis());
+        } else {
+            self.shell(&task.cmd);
+        }
+    }
+
+    fn shell(&self, cmd: &str) {
+        if Command::new("sh")
+            .args(["-c", cmd])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+            .is_err()
+        {
+            exit(1);
+        }
+    }
+
+    fn set_env_vars(&self) {
+        for (key, value) in &self.env {
+            printb!("Setting \"{}\" to \"{}\".", key, value);
+            env::set_var(key, value);
+        }
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 && args[1].eq("-v") | args[1].eq("--version") {
-        version();
-        exit(1);
-    }
+    match args.len().cmp(&1) {
+        Ordering::Equal => {
+            let recipe = Recipe::new();
+            recipe.set_env_vars();
 
-    if args.len() > 1 && args[1].eq("-h") | args[1].eq("--help") {
-        help();
-        exit(1);
-    }
-
-    if args.len() > 1 && args[1].eq("-c") | args[1].eq("--commands") {
-        print_cmds();
-        exit(1);
-    }
-
-    let recipe: Recipe = Recipe::new().unwrap();
-
-    let debug = recipe.debug.unwrap_or(false);
-
-    if recipe.env.is_some() {
-        let env = recipe.env.unwrap();
-        for (key, value) in env {
-            if debug {
-                printb!("Setting \"{}\" to \"{}\".", key, value);
+            for (name, task) in &recipe.pre {
+                recipe.execute(name, task);
             }
-            env::set_var(key, value);
-        }
-    }
 
-    if args.len() == 1 {
-        if recipe.pre.is_some() {
-            let pre = recipe.pre.unwrap();
-            for (name, p) in pre {
-                p.execute(name, debug);
-            }
-        }
-        recipe.build.execute("build".to_string(), debug);
-    }
+            recipe.execute("build", &recipe.build);
 
-    if recipe.custom.is_some() {
-        let custom = recipe.custom.unwrap();
-        for (name, c) in custom {
-            match args.len().cmp(&1) {
-                Ordering::Equal => {
-                    if c.run {
-                        c.execute(name, debug);
-                    }
-                }
-                Ordering::Less => {
-                    exit(1);
-                }
-                Ordering::Greater => {
-                    if args[1] == name {
-                        c.execute(name, debug);
-                    }
+            for (name, task) in &recipe.custom {
+                if task.run {
+                    recipe.execute(name, task);
                 }
             }
         }
-    }
-}
-
-fn run_cmd(name: String, cmd: String, debug: bool) {
-    if debug {
-        let start = SystemTime::now();
-
-        match name.as_str() {
-            "build" => {
-                println!();
-                printb!("Running build command.");
+        Ordering::Greater => match args[1].as_str() {
+            "-h" | "--help" => {
+                help();
+                exit(0);
+            }
+            "-v" | "--version" => {
+                println!("\x1b[32;1mBaker\x1b[0m ({})", env!("CARGO_PKG_VERSION"));
+                exit(0);
+            }
+            "-c" | "--commands" => {
+                print_cmds(&Recipe::new());
+                exit(0);
             }
             _ => {
-                println!();
-                printb!("Running hook \"{}\".", name);
-            }
-        }
-        run(cmd);
-        printb!("Finished in {}ms.", start.elapsed().unwrap().as_millis());
-    } else {
-        run(cmd);
-    }
+                let recipe = Recipe::new();
 
-    fn run(cmd: String) {
-        match Command::new("sh")
-            .arg("-c")
-            .arg(&cmd)
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .output()
-        {
-            Ok(_) => {}
-            Err(e) => {
-                printb!("Error: {}", e);
-                exit(1);
+                if let Some(task) = recipe.custom.get(args[1].as_str()) {
+                    recipe.set_env_vars();
+                    recipe.execute(args[1].as_str(), task);
+                } else {
+                    printb!("Command \"{}\" not found in recipe.toml.", args[1].as_str());
+                    exit(1);
+                }
             }
+        },
+        Ordering::Less => {
+            exit(1);
         }
     }
-}
-
-fn version() {
-    println!("\x1b[32m\x1b[1mBaker\x1b[0m ({})", env!("CARGO_PKG_VERSION"));
 }
 
 fn help() {
@@ -235,14 +152,9 @@ fn help() {
     println!("Link: \x1b[4m\x1b[34mhttps://github.com/rv178/baker/\x1b[0m");
 }
 
-fn print_cmds() {
-    let recipe: Recipe = Recipe::new().unwrap();
+fn print_cmds(recipe: &Recipe) {
     println!("\x1b[32mUsage: bake [command]\x1b[0m");
-    if recipe.custom.is_some() {
-        let custom = recipe.custom.unwrap();
-
-        for (name, _custom) in custom {
-            println!("\x1b[38;5;8m>\x1b[0m {}", name);
-        }
+    for name in recipe.custom.keys() {
+        println!("\x1b[38;5;8m>\x1b[0m {}", name);
     }
 }
